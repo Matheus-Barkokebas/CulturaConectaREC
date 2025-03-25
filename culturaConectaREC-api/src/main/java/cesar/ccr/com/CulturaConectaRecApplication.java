db25d6e5b80c064214f714962489d241fb47305a
Matheus <matheusbarkcaribe@gmail.com>
2025-03-25 16:55:19 -0300

    Base do BackEnd CulturaConectaREC

---
+++ culturaConectaREC-api/src/main/java/cesar/ccr/com/CulturaConectaRecApplication.java
@@ -0,0 +1,13 @@
+package cesar.ccr.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturaConectaRecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturaConectaRecApplication.class, args);
+	}
+
+}
---
+++ culturaConectaREC-api/src/test/java/cesar/ccr/com/CulturaConectaRecApplicationTests.java
@@ -0,0 +1,13 @@
+package cesar.ccr.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturaConectaRecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
